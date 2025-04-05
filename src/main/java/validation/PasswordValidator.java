d1000afd89581814450568ab8f40e8f8314b59b7
diespinola <diespinola@gmail.com>
2025-04-05 16:22:45 -0300

    Init commit to dev

---
+++ src/main/java/validation/PasswordValidator.java
@@ -0,0 +1,18 @@
+package validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
+    @Value("${password.regex}")
+    private String passwordRegex;
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        if (password == null) return false;
+        return password.matches(passwordRegex);
+    }
+}
